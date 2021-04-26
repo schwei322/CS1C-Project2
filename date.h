df861143c7e90ec8fcf646f448dbf02bedba3dd6
Dayana Pulido <andpa0520@gmail.com>
2021-04-26 06:46:43 -0700

    progress

--- date.h
+++ date.h
@@ -7,41 +7,16 @@
 #define DATE_H
 
 #include <iostream>
-#include <QString>
+#include <QDateTimeEdit>
 
 using namespace std;
 
 ///@param date class
 class date
 {
-    int month;
-    int day;
-    int year;
-    QString CompleteDate;
 
 public:
-    /// @brief Default constructor
-    date();
-    /// @brief Default constructor
-    /// @param newMonth
-    /// @param newDay
-    /// @param newYear
-    date(int newMonth, int newDay, int newYear);
-    ///@brief Destructor
-    ~date();
-
-    ///@brief Accesors
-    void setCompleteDate(QString newDate);
-    void setMonth(int newMonth);
-    void setDay(int newDay);
-    void setYear(int newYear);
-
-
-    ///@brief Mutators
-    QString getCompleteDate(QString completeDate, int day, int month, int year);
-    int getMonth();
-    int getDay();
-    int getYear();
+    PurchaseData queryWithDate(QDate date);
 
 };
 
